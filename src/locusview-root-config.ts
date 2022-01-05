0842c7d74e9b93693cf976f86aa78552d5e10ddf
maximlivshitz <maxim.livshitz@locusview.com>
2022-01-05 09:39:59 +0200

    commit-2

--- src/locusview-root-config.ts
+++ src/locusview-root-config.ts
@@ -1,5 +1,7 @@
 import {
   registerApplication,
+  unregisterApplication,
+  unloadApplication,
   start,
   navigateToUrl,
   RegisterApplicationConfig,
@@ -12,11 +14,9 @@ import {
 
 // import 'zone.js';
 
-const showWhenAnyOf = (routes) => {
+const showWhenAnyOf = (routes, appName) => {
   return function (location) {
-    return routes.some((route) => {
-      return location.pathname === route;
-    });
+    return routes.some((route) => location.pathname === route);
   };
 };
 
@@ -37,56 +37,47 @@ const showExcept = (routes) => {
 // registerApplication('first-angular-app', () => System.import('@locusview/angular-app'), showWhenPrefix(['/first-angular-app']));
 // registerApplication('first-react-app', () => System.import('@locusview/react-app'), showWhenPrefix(['/first-react-app']));
 
+const loginApp: RegisterApplicationConfig = {
+  name: "login",
+  app: () => System.import("@locusview/auth-app"),
+  activeWhen: showWhenAnyOf(["/", "/login", "/signin"], "login"),
+};
+
+const layoutApp: RegisterApplicationConfig = {
+  name: "layout",
+  app: () => System.import("@locusview/layout-app"),
+  activeWhen: ["/first-angular-app", "/first-react-app"],
+  customProps: (name, location) => {
+    return { authToken: "d83jD63UdZ6RS6f70D0" };
+  },
+};
+
+const firstAngularApp: RegisterApplicationConfig = {
+  name: "first-angular-app",
+  app: () => System.import("@locusview/angular-app"),
+  activeWhen: showWhenPrefix(["/first-angular-app"]),
+  customProps: (name, location) => {
+    return { authToken: "d83jD63UdZ6RS6f70D0" };
+  },
+};
+
+const firstReactApp: RegisterApplicationConfig = {
+  name: "first-react-app",
+  app: () => System.import("@locusview/react-app"),
+  activeWhen: showWhenPrefix(["/first-react-app"]),
+  customProps: (name, location) => {
+    return { authToken: "d83jD63UdZ6RS6f70D0" };
+  },
+};
+
 const applications: RegisterApplicationConfig[] = [
-  {
-    name: "layout",
-    app: () => System.import("@locusview/layout-app"),
-    activeWhen: ["/first-angular-app", "/first-react-app"],
-  },
-  {
-    name: "login",
-    app: () => System.import("@locusview/auth-app"),
-    activeWhen: showWhenAnyOf(["/", "/login", "/signin"]),
-  },
-  {
-    name: "first-angular-app",
-    app: () => System.import("@locusview/angular-app"),
-    activeWhen: showWhenPrefix(["/first-angular-app"]),
-  },
-  {
-    name: "first-react-app",
-    app: () => System.import("@locusview/react-app"),
-    activeWhen: showWhenPrefix(["/first-react-app"]),
-  },
+  loginApp,
+  layoutApp,
+  firstAngularApp,
+  firstReactApp,
 ];
 
 applications.forEach((www) => registerApplication(www));
-
-// HTML LAYOUT
-
-// const routes = constructRoutes(document.querySelector('#single-spa-layout'));
-// JSON LAYOUT
-// const routes = constructRoutes({
-//     type: "route",
-//     path: "",
-//     props: {title: "Title"},
-//     routes: [{
-//         type: "application",
-//         name: "@locusview/navbar",
-//         props: {title: "Title"},
-//     }],
-//     default: true
-// });
-
-// const applications = constructApplications({
-//     routes,
-//     loadApp({name}) {
-//         return System.import(name);
-//     },
-// });
-//
-// const layoutEngine = constructLayoutEngine({routes, applications});
-// applications.forEach(registerApplication);
 
 start({
   urlRerouteOnly: true,
